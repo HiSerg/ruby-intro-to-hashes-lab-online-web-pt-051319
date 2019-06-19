c2f56b7ec946dd09f6d996e9122f85b78855bb87
Sergio Guzman <hiserg98@gmail.com>
2019-06-19 02:43:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,9 +13,9 @@ end
 
 def monopoly_with_second_tier
   monopoly = {:railroads =>{:pieces => 4, 
-  :rent_in_dollars => {}
-  :name => {},
-  
+  :rent_in_dollars => {},
+  :name => {}
+    }
   }
 
 end
